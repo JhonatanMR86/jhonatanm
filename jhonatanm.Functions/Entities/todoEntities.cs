4ccbd3bbb1597dd8343b2448bdd0f45d15c22224
JHONATAN ALEXANDER MONSALVE RENDON <jhonatanmonsalve130089@correo.itm.edu.co>
2021-08-30 23:57:19 -0400

    add Entity

---
+++ jhonatanm.Functions/Entities/todoEntities.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jhonatanm.Functions.Entities
+{
+    public class todoEntities : TableEntity
+    {
+        public int Employed { get; set; }
+
+        public DateTime date { get; set; }
+
+        public int Type { get; set; }
+
+        public bool Consolidated { get; set; }
+    }
+}
---
+++ jhonatanm.common/Models/todo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace jhonatanm.common.Modelos
+{
+    internal class todo
+    {
+        public int Employed { get; set; }
+
+        public DateTime date { get; set; }
+
+        public int Type { get; set; }
+
+        public bool Consolidated { get; set; }
+    } 
+}
